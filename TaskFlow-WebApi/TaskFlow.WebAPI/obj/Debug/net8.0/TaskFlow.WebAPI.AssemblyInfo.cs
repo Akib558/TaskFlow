d46bd777699ca1e1345992bb187976384cc3685e
Akib558 <xenonsia1234@gmail.com>
2024-11-14 00:54:13 +0600

    LogginMiddlewae imeplemented

--- TaskFlow-WebApi/TaskFlow.WebAPI/obj/Debug/net8.0/TaskFlow.WebAPI.AssemblyInfo.cs
+++ TaskFlow-WebApi/TaskFlow.WebAPI/obj/Debug/net8.0/TaskFlow.WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa14a075da3652bfaad500c032187fda501a8f06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e261d5342f85ec39d6cf04fdf9d7ba25555144b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

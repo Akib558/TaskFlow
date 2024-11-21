686e80980e9004bb84a7c545506e8bd3c3778460
Akib558 <xenonsia1234@gmail.com>
2024-11-22 01:32:54 +0600

    Guid Added in usertable

--- TaskFlow-WebApi/TaskFlow.Data/Entities/UserEntity.cs
+++ TaskFlow-WebApi/TaskFlow.Data/Entities/UserEntity.cs
@@ -3,7 +3,9 @@ namespace TaskFlow.Data.Entities;
 public class UserEntity
 {
     public int Id { get; set; }
+    public string GuidId { get; set; }
     public string Username { get; set; }
+    public string Email { get; set; }
     public string PasswordHash { get; set; }
     public string Role { get; set; }
     public DateTime CreatedDate { get; set; } = DateTime.UtcNow;

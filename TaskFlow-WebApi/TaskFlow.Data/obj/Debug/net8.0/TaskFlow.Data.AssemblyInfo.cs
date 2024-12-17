8dbb7915af5468c5fc80491302293d3ef8f67649
Akib558 <xenonsia1234@gmail.com>
2024-12-18 00:00:55 +0600

    routine commit

--- TaskFlow-WebApi/TaskFlow.Data/obj/Debug/net8.0/TaskFlow.Data.AssemblyInfo.cs
+++ TaskFlow-WebApi/TaskFlow.Data/obj/Debug/net8.0/TaskFlow.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a4ab229c3d4cc09206d15131609d475cf4f9688")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c18e99c41148e14b1a59710ee0800a915773c265")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
